c204dd4f28cf305de72b8dca74e1d60a5d99c8db
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-09-20 18:42:33 +0200

    Finalize next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const basePath=process.env.NODE_ENV==="production"?"/nordhus":"";
-const nextConfig={
-  output:"export",
+const basePath = process.env.NODE_ENV === "production" ? "/nordhus" : "";
+const nextConfig = {
+  output: "export",
   basePath,
-  images:{unoptimized:true},
-  env:{NEXT_PUBLIC_BASE_PATH:basePath},
+  images: { unoptimized: true },
+  env: { NEXT_PUBLIC_BASE_PATH: basePath },
 };
 export default nextConfig;

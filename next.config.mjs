ded3b34670eae9ef618fd3612a371551a50ba638
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-09-20 15:37:55 +0200

    Configure static GitHub Pages preview

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
+  ...(isGitHubPages ? {
+    output: "export",
+    basePath: "/nordhus",
+    assetPrefix: "/nordhus/",
+    trailingSlash: true,
+  } : {}),
   images: {
     formats: ["image/avif", "image/webp"],
     qualities: [75, 90],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 2560],
+    ...(isGitHubPages ? { unoptimized: true } : {}),
   },
 };
 

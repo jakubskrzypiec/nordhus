8443c9efe9d4b44514605ca3672efbbaac28d15a
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-09-20 18:42:59 +0200

    Finalize static hero

--- components/Hero.js
+++ components/Hero.js
@@ -2,11 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import Container from "./Container";
 import {BLUR_DATA_URL} from "@/lib/image";
-import {asset} from "@/lib/asset";
 
 export default function Hero({image,title,text,primary,secondary,alt=""}){
  return <section className="hero-shell relative min-h-svh overflow-hidden bg-black text-white">
-  <Image src={asset(image)} alt={alt} fill priority className="object-cover" sizes="100vw" placeholder="blur" blurDataURL={BLUR_DATA_URL}/>
+  <Image src={image} alt={alt} fill priority className="object-cover" sizes="100vw" placeholder="blur" blurDataURL={BLUR_DATA_URL}/>
   <div className="absolute inset-0 bg-black/40"/>
   <Container className="relative z-10 flex min-h-svh items-end pb-12 pt-36 md:pb-20">
    <div className="max-w-4xl">

daa0e873473e45c0c24231906ef21d7689fbd0c6
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-09-20 14:20:12 +0200

    Improve full-size image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig={images:{formats:["image/avif","image/webp"]}};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    qualities: [75, 90],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 2560],
+  },
+};
+
 export default nextConfig;

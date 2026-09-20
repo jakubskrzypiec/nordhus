aebd43790b7f470fe9571f7133c9914b7e3a5a64
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-09-20 15:10:34 +0200

    Set editorial grid width to 1360px

--- components/Container.js
+++ components/Container.js
@@ -1,1 +1,1 @@
-export default function Container({children,className="",as:Tag="div"}){return <Tag className={`mx-auto w-full max-w-[1280px] px-5 sm:px-8 lg:px-12 ${className}`}>{children}</Tag>}
+export default function Container({children,className="",as:Tag="div"}){return <Tag className={`mx-auto w-full max-w-[1360px] px-5 sm:px-8 lg:px-12 ${className}`}>{children}</Tag>}
